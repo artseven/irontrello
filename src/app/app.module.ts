0590b8a4c5b3da5a78208477365a3007d8678b06
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-15 15:43:52 -0400

    session service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Http } from '@angular/http'
+import 'rxjs/Rx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -4,13 +4,14 @@ import 'rjxs/add/operator/toPromise';
 
 @Injectable()
 export class SessionService {
+  baseUrl: string ="http://localhost:3000";
 
   constructor(
     private myHttpThang: Http
   ) { }
 
   checkLogin() {
-    return this.myHttpThang.get('/checklogin')
+    return this.myHttpThang.get(this.baseUrl + '/checklogin')
     .toPromise()
     .then(res => res.json());
   }
