78bd88699f27417c8c0b625ed58879a30c0d3602
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-15 15:09:23 -0400

    Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ListsPageComponent } from './lists-page/lists-page.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ListsPageComponent,
+    LogInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
