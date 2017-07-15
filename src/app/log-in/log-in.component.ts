393dbfd655a37e40a5ce1d601bd0b663df429807
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-15 16:18:14 -0400

    log in

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SessionService } from '../services/session.service';
+
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
@@ -9,9 +11,22 @@ export class LogInComponent implements OnInit {
   formEmail: string;
   formPassword: string;
 
-  constructor() { }
+  constructor(
+    private sessionThang: SessionService
+  ) { }
 
   ngOnInit() {
+  }
+
+  submitLogin() {
+    this.sessionThang.login(this.formEmail, this.formPassword )
+      .then(() => {
+        alert('ZOMG login success');
+      })
+      .catch((theError) => {
+        console.log(theError);
+        alert('ERROR');
+      });
   }
 
 }
